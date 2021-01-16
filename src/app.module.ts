21fb89603e39cbb1ec33c7767c51fb775f2a022d
James <jhyu0210@gmail.com>
2021-01-16 16:48:22 +0900

    change schema.gql file location into src

--- src/app.module.ts
+++ src/app.module.ts
@@ -8,7 +8,7 @@ import { CatsModule } from './cats/cats.module';
   imports: [  
     CatsModule,
     GraphQLModule.forRoot({
-    autoSchemaFile: 'schema.gql',
+    autoSchemaFile: 'src/schema.gql',
     sortSchema: true,
   })],
   controllers: [AppController],
